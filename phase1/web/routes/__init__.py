952368deabe495bda13b20f32ebd6fc26257e604
Dino Drčec <dino.drcec46@gmail.com>
2025-12-11 00:17:19 +0100

    Faza 1 - update

--- phase1/web/routes/__init__.py
+++ phase1/web/routes/__init__.py
@@ -1,15 +1,4 @@
-# web/models/__init__.py
-from .shipment import Shipment
-
-__all__ = ['Shipment']
-
-# web/routes/__init__.py
 from .shipments import shipments_bp
 from .tracking import tracking_bp
 
 __all__ = ['shipments_bp', 'tracking_bp']
-
-# web/services/__init__.py
-from .mongodb_service import MongoDBService
-
-__all__ = ['MongoDBService']
